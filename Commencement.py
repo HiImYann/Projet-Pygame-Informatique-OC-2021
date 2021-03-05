454ad91e4497c4da17012cc84c74b8a6996970b5
HiImYann <48321843+HiImYann@users.noreply.github.com>
2021-03-05 10:44:42 +0100

    Cercle rouge qui bouge

--- Commencement.py
+++ Commencement.py
@@ -3,16 +3,35 @@ pygame.init()
 
 screen = pygame.display.set_mode([800,800])
 
+clock = pygame.time.Clock()
+fps_limit = 60.0
+
+posx = 400
+posy = 400
+
 running = True
 while running:
+    clock.tick(fps_limit)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((255,255,255))
-
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
+    circle = pygame.draw.circle(screen, (255,0,0), (posx, posy), 100)
     pygame.display.flip()
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            posx = posx - 10
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            posx = posx + 10
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            posy = posy - 10
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_DOWN:
+            posy = posy + 10
     
 pygame.quit()
