96578e0cc3e4ed35d3f457177c136d8337eb7408
HiImYann <48321843+HiImYann@users.noreply.github.com>
2021-03-05 09:25:54 +0100

    Fenêtre ouverte sur pygame

---
+++ Commencement.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([800,800])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255,255,255))
