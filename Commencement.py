b29ff788311e855fc89eddde36723c7a77f382f2
HiImYann <leon07212@outlook.com>
2021-03-06 17:30:55 +0100

    LA FONCTION CERLCE MARCHE (Merci Quentin)

--- Commencement.py
+++ Commencement.py
@@ -8,14 +8,14 @@ fps_limit = 60.0
 screen = pygame.display.set_mode([800,800])
 pygame.display.set_caption('Cul gras')
 
-centre = (400,400)
 posx = 400
 posy = 400
+centre = (posx,posy)
 
-#Vitesse linéare / vitesse diagonale
+#Vitesse
 vit = 10
-vitd = 5 
 
+red = (255,0,0)
 
 class Cercle:
     def __init__(self,color,radius,surface=screen,center=centre):
@@ -25,6 +25,8 @@ class Cercle:
         self.radius = radius
     def dessin_cercle(self):
         pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+
+cercle1 = Cercle(red,75,screen,(posx,posy))
 
 running = True
 while running:
@@ -36,7 +38,7 @@ while running:
 
     screen.fill((255,255,255))
     #circle = pygame.draw.circle(screen, (255,0,0), (posx, posy), 100)
-    circle1 = dessin_cercle(screen, (255,0,0), centre, 75)
+    cercle1.dessin_cercle()
     
     i = 0
     N = 0
@@ -50,15 +52,19 @@ while running:
     
     if keys[pygame.K_LEFT]:
         posx -= vit
+        print("gauche")
 
     if keys[pygame.K_RIGHT]:
         posx += vit
+        print("droite")
 
     if keys[pygame.K_DOWN]:
         posy += vit
+        print("bas")
 
     if keys[pygame.K_UP]:
         posy -= vit
+        print("haut")
 
     pygame.display.flip()
     
