4308b524a285731ef60bbfcd1884570af3b19a09
HiImYann <48321843+HiImYann@users.noreply.github.com>
2021-03-06 13:14:07 +0100

    Cercle bouge diagonale
    Le cercle peut bouger en diagonale. J'ai aussi fait apparaître 10 cubes avec une boucle for mais ils sont inutiles car ils s'affichent mais ils ne sont pas des objets

--- Commencement.py
+++ Commencement.py
@@ -1,13 +1,29 @@
 import pygame
+import time
 pygame.init()
-
-screen = pygame.display.set_mode([800,800])
 
 clock = pygame.time.Clock()
 fps_limit = 60.0
 
+screen = pygame.display.set_mode([800,800])
+pygame.display.set_caption('Cul gras', clock)
+
 posx = 400
 posy = 400
+
+#Vitesse linéare / vitesse diagonale
+vit = 10 
+vitd = 5 
+
+color = (255,0,0)
+radius = 10
+
+'''
+class Cercle:
+    def __init__(self,color,radius):
+        self.color = color
+        self.radius = radius
+    pygame.draw.circle(screen, color, (400,400), radius)'''
 
 running = True
 while running:
@@ -19,19 +35,30 @@ while running:
 
     screen.fill((255,255,255))
     circle = pygame.draw.circle(screen, (255,0,0), (posx, posy), 100)
-    pygame.display.flip()
+    '''circle1 = Cercle((255,0,0),100)'''
+    
+    i = 0
+    N = 0
+    for i in range(10):
+        pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(1+N, 1+N, 60, 60))
+        i += 1
+        N += 82
 
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            posx = posx - 10
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_RIGHT:
-            posx = posx + 10
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_UP:
-            posy = posy - 10
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_DOWN:
-            posy = posy + 10
+
+    keys = pygame.key.get_pressed() 
+    
+    if keys[pygame.K_LEFT]:
+        posx -= vit
+
+    if keys[pygame.K_RIGHT]:
+        posx += vit
+
+    if keys[pygame.K_DOWN]:
+        posy += vit
+
+    if keys[pygame.K_UP]:
+        posy -= vit
+
+    pygame.display.flip()
     
 pygame.quit()
