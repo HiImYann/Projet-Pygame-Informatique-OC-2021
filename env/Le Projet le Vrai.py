e108c93de604281903d28e0c9c53cbe39240ab7f
HiImYann <leon07212@outlook.com>
2021-05-21 13:40:25 +0200

    Création du fichier final et modification des images

---
+++ env/Le Projet le Vrai.py
@@ -0,0 +1,77 @@
+import pygame, random
+
+# -- Variables --
+
+pygame.init()
+clock = pygame.time.Clock()
+fps_limit = 60.0
+
+resx = 626 #Dimensions of the window, aspect ratio of 16/9
+resy = 563
+screen = pygame.display.set_mode([resx,resy]) #Defines the window's size, here 1600x900 pixels
+
+next_particle_time = 0
+
+background_image = pygame.image.load(r'C:\Users\yannd\OneDrive\Projet Pygame Informatique OC 2021\env\background_image_no_clouds.jpg')
+
+
+# -- Classes --
+
+class Particle():
+    def __init__ (self,color,radius):
+        pygame.sprite.Sprite.__init__(self)
+        self.color = color
+        self.radius = radius
+        self.position_x = random.randint(radius, 1600-radius)
+        self.position_y = random.randint(radius, 900-radius)
+
+    def update(self):
+        pygame.draw.circle(screen, self.color, (self.position_x,self.position_y),self.radius)
+
+    def random_movement(self):
+        self.position_x += random.randint(0,5)
+        self.position_y -= random.randint(0,5)
+
+    def life(self):
+        self.radius -= 1
+
+# -- Objets --
+
+particle1 = Particle((255,0,0),100)
+
+# -- Lists --
+
+particles = []
+
+# -- Game loop --
+
+running = True
+while running:
+    clock.tick(fps_limit) #Limite le nombre d'images par seconde du jeu à 60
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #screen.fill((0,0,0))
+    screen.blit(background_image, [0, 0])
+    random_color = (255,random.randint(0,180),0)
+
+
+    current_time = pygame.time.get_ticks()
+    if current_time > next_particle_time:
+        particles.append(Particle(random_color,random.randint(0,100)))
+        next_particle_time = current_time + 10
+
+    for p in particles[:]:
+        p.update()
+        p.random_movement()
+        p.life()
+        if p.radius < 1:
+            particles.remove(p)
+    print(len(particles))
+
+    pygame.display.set_caption("Le feuf    FPS : "+"{:.2f}".format(clock.get_fps())+"    n Particules : "+str(len(particles)))
+    #pygame.display.set_caption("{:.2f}".format(clock.get_fps()))
+
+    pygame.display.flip()   
+pygame.quit()
